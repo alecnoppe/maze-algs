2b470d9c453beaa32815a95e0169a9a4d7470ccc
alecnoppe <79417933+alecnoppe@users.noreply.github.com>
2024-06-25 22:51:48 +0200

    add: Prims algorithm for maze generation

--- generators/__init__.py
+++ generators/__init__.py
@@ -1,5 +1,6 @@
 from .randomized_DFS import RandomizedDFS
 from .wilson import Wilson
 from.fractal_tessellation import FractalTessellation
+from .prim import Prim
 
-__all__ = ['RandomizedDFS', 'Wilson', 'FractalTessellation']
+__all__ = ['RandomizedDFS', 'Wilson', 'FractalTessellation', 'Prim']
